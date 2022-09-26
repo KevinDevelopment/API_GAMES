057cf20244e30e16842b2e31b09a5714db45f654
kevin <kevincristhian@outlook.com>
2022-09-26 17:47:03 -0300

    :sparkles: feat: first commit

---
+++ controller/ListControler.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Games = require("../model/Games");
+
+router.get("/games", (request, response) => {
+  Games.findAll({ raw: true }).then((games) => {
+    return response.status(200).json({
+      games: games
+    })
+  }).catch((erro) => {
+    console.log("error")
+  })
+});
+
+module.exports = router;
